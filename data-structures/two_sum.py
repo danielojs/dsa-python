d432b784d2a5d74590a4ea6a8c8a432c70fc5a22
danielojs <danielojsandil@gmail.com>
2026-04-03 23:33:37 +0700

    feat: complete two sum problem

--- data-structures/two_sum.py
+++ data-structures/two_sum.py
@@ -1,14 +1,25 @@
 # Arrays Practice Problem
 
 nums = [2, 7, 11, 15]
-target = 9
 
-output = []
-for i in range(len(nums)):
-    for j in range(len(nums)):
-        if i != j:
-            if nums[i] + nums[j] == target:
-                output = [i, j]
-                break
-print(output)
+def two_sum(arr, target):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if i != j:
+                if arr[i] + arr[j] == target:
+                    return [i, j]
+
+print(two_sum(nums, 10))
+
+
+def two_sum2(arr, target):
+    seen = {}
+    for i, num in enumerate(arr):
+        complement = target - num
+        if complement in seen:
+            print(seen)
+            return [seen[complement], i]
+        seen[num] = i
+
+print(two_sum2(nums, 17))
 
