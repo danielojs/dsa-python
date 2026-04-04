5b5e7f2daf95bdda642781adf4e463750eea4448
danielojs <danielojsandil@gmail.com>
2026-04-04 10:58:44 +0700

    feat: done contains duplicate

---
+++ data-structures/contains_duplicate.py
@@ -0,0 +1,14 @@
+# Contains Duplicate Problem
+
+nums = [1, 2, 3, 4]
+
+def contains_duplicate(arr) -> bool:
+    unique_nums = set()
+    for num in arr:
+        if num in unique_nums:
+            return True
+        unique_nums.add(num)
+    return False
+
+print(contains_duplicate(nums))
+
